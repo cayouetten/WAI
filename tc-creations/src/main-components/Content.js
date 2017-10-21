ac25395a4a7c9ca5916ee9c02a215bcaa53cff2f
Nicole Cayouette <nicole.cay@yahoo.com>
2017-10-21 13:04:35 -0400

    Style Updates
    All pages and views, style edits and efficiency

---
+++ tc-creations/src/main-components/Content.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Blog from '../pages/Blog';
+import Tutorials from '../pages/Tutorials';
+import Recipes from '../pages/Recipes';
+import Contact from '../pages/Contact';
+
+import '../css/App.css';
+import '../css/main-components/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className='Content'>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/about' component={ About }  />
+          <Route path='/blog' component={ Blog }  />
+          <Route path='/tutorials' component={ Tutorials }  />
+          <Route path='/recipes' component={ Recipes }  />
+          <Route path='/contact' component={ Contact }  />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ tc-creations/src/main-components/Navigation.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import '../css/App.css';
+import '../css/main-components/Navigation.css';
+
+import logo from '../images/logos/logo-sm-white.svg';
+import menu from '../images/icons/mobile-nav-icon.svg';
+import exit from '../images/icons/mobile-nav-exit.svg';
+
+class Navigation extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth,
+      mobileNavVisible: false
+    };
+  }
+  
+  handleResize(){
+    this.setState({
+      windowWidth: window.innerWidth
+    });
+  }
+  
+  componentDidMount(){
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
+  
+  componentWillUnmount(){
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
+  
+  navigationLinks(){
+    return [
+      <ul className='nav-links-list'>
+        <li><NavLink activeClassName='active' onClick={this.handleNavClick.bind(this)} exact to='/'>HOME</NavLink></li>
+        <li><NavLink activeClassName='active' onClick={this.handleNavClick.bind(this)} to='/about'>ABOUT</NavLink></li>
+        <li><NavLink activeClassName='active' onClick={this.handleNavClick.bind(this)} to='/blog'>BLOG</NavLink></li>
+        <li><NavLink activeClassName='active' onClick={this.handleNavClick.bind(this)} to='/tutorials'>TUTORIALS</NavLink></li>
+        <li><NavLink activeClassName='active' onClick={this.handleNavClick.bind(this)} to='/recipes'>RECIPES</NavLink></li>
+        <li><NavLink activeClassName='active' onClick={this.handleNavClick.bind(this)} to='/contact'>CONTACT</NavLink></li>
+      </ul>
+    ];
+  }
+  
+  mobileNavRender(){
+    if(this.state.mobileNavVisible){
+      return this.navigationLinks();
+    }
+  }
+  
+  handleNavClick(){
+    if(!this.state.mobileNavVisible){
+      this.setState({mobileNavVisible: true});
+    } else {
+      this.setState({mobileNavVisible: false});
+    }
+  }
+  
+  mobileButtonToggle(){
+    if(!this.state.mobileNavVisible){
+      return <img src={menu} onClick={this.handleNavClick.bind(this)} className='nav-menu' alt='menu' />
+    } else {
+      return <img src={exit} onClick={this.handleNavClick.bind(this)} className='nav-menu' alt='exit' />
+    }
+  }
+  
+  navigationLinksRender(){
+    if(this.state.windowWidth < 800){
+      return [
+        <div className='nav-mobile'>
+          {this.mobileButtonToggle()}
+          {this.mobileNavRender()}
+        </div>
+      ];
+    } else {
+      return [
+        <div className='nav-web'>
+          {this.navigationLinks()}
+        </div>
+      ];
+    }
+  }
+  
+  render(){
+    return (
+      <div className='Navigation'>
+        <img src={logo} className='nav-logo' alt='logo' />
+        {this.navigationLinksRender()}
+      </div>
+    );
+  }
+}
+
+export default Navigation;
